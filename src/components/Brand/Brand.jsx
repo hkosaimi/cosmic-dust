d35c6082f2a0227413b4a5a66b8746eb0be75c5f
Hussain <hkosaimi@gmail.com>
2023-08-01 19:29:14 +0300

    new commit

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,8 +1,28 @@
 import "./Brand.css";
+import { google, dropbox, atlassian, slack, shopify } from "./imports";
 function Brand() {
   return (
     <>
-      <div className="brand"></div>
+      <div className="brand">
+        <div className="brand__slide">
+          <img src={google} />
+          <img src={dropbox} />
+          <img src={atlassian} />
+          <img src={slack} />
+          <img src={shopify} />
+          <img src={google} />
+          <img src={dropbox} />
+        </div>
+        <div className="brand__slide">
+          <img src={google} />
+          <img src={dropbox} />
+          <img src={atlassian} />
+          <img src={slack} />
+          <img src={shopify} />
+          <img src={google} />
+          <img src={dropbox} />
+        </div>
+      </div>
     </>
   );
 }
