708e365e60b57d5c45706d16cfaa6ddb046534e9
Hussain <hkosaimi@gmail.com>
2023-08-01 21:20:49 +0300

    new

--- src/components/Templates/Templates.jsx
+++ src/components/Templates/Templates.jsx
@@ -3,6 +3,8 @@ import t1 from "../../assets/1.png";
 import t2 from "../../assets/2.png";
 import t3 from "../../assets/3.png";
 import t4 from "../../assets/4.png";
+import t5 from "../../assets/5.png";
+import t6 from "../../assets/6.png";
 function Templates() {
   return (
     <>
@@ -18,6 +20,12 @@ function Templates() {
         </div>
         <div className="templates__pics">
           <img src={t4} />
+        </div>
+        <div className="templates__pics">
+          <img src={t5} />
+        </div>
+        <div className="templates__pics">
+          <img src={t6} />
         </div>
       </div>
     </>
